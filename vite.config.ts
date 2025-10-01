80bd7bbc177dfbf7348371ce70c6c3d5229f0358
ZhuGG <h.bettollo@gmail.com>
2025-10-01 17:02:26 +0200

    Use relative base path for production builds

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Th-o/' : '/',
+  base: mode === 'production' ? './' : '/',
   plugins: [react()],
 }));
